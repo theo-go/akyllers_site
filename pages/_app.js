1101dcf707ad9b4f1e7a25ad02ebab053093ebdf
Uros Randelovic <urandjelovic@microsoft.com>
2022-06-08 11:38:17 +0200

    - fix for audio player

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,28 @@
 import '../styles/globals.css'
-import React, { useState, Suspense  } from 'react';
+import React from 'react';
 
 import { PlasmicRootProvider } from "@plasmicapp/react-web";
 // import OuterLayout from './outerlayout';
 import "./images/audio-player-styles.css"
 
 import AppContextProvider from '../components/AppContext';
+import OuterLayout from "./outerlayout";
 
 // import LoadingScreen from '../components/LoadingScreen'
 
 
-const OuterLayout = React.lazy(() => import('./outerlayout'));
+
 
 function MyApp({ Component, pageProps }) {
 
 
   return (
     <AppContextProvider>
-      <Suspense fallback={<div>Loading...</div>}>
       <OuterLayout>
         <PlasmicRootProvider>
           <Component {...pageProps} />
         </PlasmicRootProvider>
       </OuterLayout>
-      </Suspense>
     </AppContextProvider>
   );
 }
--- pages/outerlayout.jsx
+++ pages/outerlayout.jsx
@@ -11,6 +11,18 @@ import PlayButton from "./images/play.png";
 import PauseButton from "./images/pause.png";
 
 
+
+const allTunes = [
+    "https://firebasestorage.googleapis.com/v0/b/lexyom-site.appspot.com/o/mp3s%2FTales%20Of%20Pharaohs%2C%20Balloonists%2C%20Lovers%20And%20Yogis.mp3?alt=media&token=77f54d1f-f9eb-4fd8-88f8-658999051a3d",
+    "https://firebasestorage.googleapis.com/v0/b/lexyom-site.appspot.com/o/mp3s%2FResonate.mp3?alt=media&token=68389111-be75-453f-b19a-76fdc57ab5fb",
+    "https://firebasestorage.googleapis.com/v0/b/lexyom-site.appspot.com/o/mp3s%2FMist%20After%20Smoke.mp3?alt=media&token=5614749f-e69e-452e-9212-e461eeacd959"
+    // "https://firebasestorage.googleapis.com/v0/b/lexyom-site.appspot.com/o/mp3s%2FFragile.mp3?alt=media&token=18f8f833-c159-4539-b765-ab3bdb458975",
+    // "https://firebasestorage.googleapis.com/v0/b/lexyom-site.appspot.com/o/mp3s%2F2pm%20Seems%20Like%20an%20Hour%20Ago.mp3?alt=media&token=bdd23806-5a32-4504-a37b-22b9df736f58"
+]
+let tuneIndex = 0
+let playlist = []
+let currTune = 'blah'
+
 const playIcon = (
     <Image src={PlayButton} />
 )
@@ -23,74 +35,65 @@ const parentWrapper = {
     // display: 'flex'
 }
 
+function handlePlayNext() {
+    if (tuneIndex >= playlist.length) {
+        // dispatch({tuneIndex: 0})
+        tuneIndex=0
+    } else {
+        // dispatch({tuneIndex: tuneIndex+1})
+        // dispatch({currTune: playlist[tuneIndex]})
+        console.log(tuneIndex)
+        console.log(currTune)
+    }
+
+};
+
+function handleClickPrevious () {
+    if (tuneIndex === 0) {
+        dispatch({tuneIndex: 0})
+    } else {
+        dispatch({tuneIndex: tuneIndex-1})
+        dispatch({currTune: playlist[tuneIndex]})
+        console.log(tuneIndex)
+        console.log(currTune)
+    }
+
+};
+
+const Player = () => (
+    <AudioPlayer
+        layout="horizontal"
+        // autoPlay={true}
+        // autoPlayAfterSrcChange={true}
+        muted={false}
+        volume={0.7}
+        showJumpControls={false}
+        showSkipControls={true}
+        customAdditionalControls={[]}
+        customProgressBarSection={[]}
+        customControlsSection={[RHAP_UI.MAIN_CONTROLS]}
+        customVolumeControls={[]}
+        customIcons={{
+            play: playIcon,
+            pause: pauseIcon
+        }}
+
+        src={"https://firebasestorage.googleapis.com/v0/b/lexyom-site.appspot.com/o/mp3s%2FTales%20Of%20Pharaohs%2C%20Balloonists%2C%20Lovers%20And%20Yogis.mp3?alt=media&token=77f54d1f-f9eb-4fd8-88f8-658999051a3d"}
+        // onEnded={handlePlayNext}
+        // onClickPrevious={handleClickPrevious}
+        // onClickNext={handlePlayNext}
+        // other props here
+    />
+);
+
+
+
 
 function OuterLayout({ children }) {
-    const {state: {currTune}, dispatch } = useContext(AppContext)
-
-
-
-    // useEffect(() => {
-    //     setPlaylist(allTunes);
-    //     setCurrTune(allTunes[0]);
-    //     console.log('use effect triggered')
-    // }, []);
-
-
-    function handlePlayNext() {
-        if (tuneIndex >= playlist.length) {
-            setTuneIndex(0)
-        } else {
-            setTuneIndex(tuneIndex + 1)
-            setCurrTune(playlist[tuneIndex])
-            console.log(tuneIndex)
-            console.log(state.currTune)
-        }
-
-    };
-
-    function handleClickPrevious () {
-        if (tuneIndex === 0) {
-            setTuneIndex(0)
-        } else {
-            setTuneIndex(tuneIndex - 1)
-            setCurrTune(playlist[tuneIndex])
-            console.log(tuneIndex)
-            console.log(currTune)
-        }
-
-    };
-
-
-    const Player = () => (
-        <AudioPlayer
-            layout="horizontal"
-            // autoPlay={true}
-            // autoPlayAfterSrcChange={true}
-            muted={false}
-            volume={0.7}
-            showJumpControls={false}
-            showSkipControls={true}
-            customAdditionalControls={[]}
-            customProgressBarSection={[]}
-            customControlsSection={[RHAP_UI.MAIN_CONTROLS]}
-            customVolumeControls={[]}
-            customIcons={{
-                play: playIcon,
-                pause: pauseIcon
-            }}
-    
-            src={currTune}
-            onEnded={handlePlayNext}
-            onClickPrevious={handleClickPrevious}
-            onClickNext={handlePlayNext}
-        // other props here
-        />
-    );
-
     return (
         <div style={parentWrapper}>
-            <Player />
             <main>{children}</main>
+            <Player />
         </div>
     )
 }
